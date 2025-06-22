1b60bf447776427df159e140f98730d61b5ca6ee
Senie Calalang <senie.c88@gmail.com>
2025-06-22 18:15:14 +0900

    created livehouse model with location, genres, address, description

---
+++ server/models/livehouse.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import User from './models/user.model.js';
+import Band from './models/band.model.js';
+
+const { Schema } = mongoose;
+
+const LivehouseSchema = new Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    band: { type: mongoose.Schema.Types.ObjectId, ref: 'Band' },
+
+    name: String,
+    address: String,                                       
+    description: String,
+    genres: [{ type: String, trim: true }],
+    location: {
+        type: {
+            // Required for GeoJSON
+            type: String,
+            enum: ['Point'],
+            default: 'Point',
+            required: true
+        },
+        coordinates: {
+            // Required for geospatial queries
+            type: [Number], // [longitude, latitude]
+            required: true
+        }
+    }
+});
+
+export default mongoose.model('Livehouse', LivehouseSchema);
