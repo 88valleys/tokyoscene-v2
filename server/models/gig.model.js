b0b44fcaa3d5739fa7e779d320994b15645ac9a9
Senie Calalang <senie.c88@gmail.com>
2025-06-22 17:26:56 +0900

    added user, band, livehouse imports and cleaned up values in gig model

--- server/models/gig.model.js
+++ server/models/gig.model.js
@@ -1,24 +1,24 @@
 import mongoose from 'mongoose';
 
+import User from './models/user.model.js';
+import Band from './models/band.model.js';
+import Livehouse from './models/livehouse.model.js';
+
 const { Schema } = mongoose;
 
 const GigSchema = new Schema({
-    location: {
-        name: String,
-        latitude: String,
-        longitude: String
-    },
+    created_by: { type: Schema.Types.ObjectId, ref: 'User' },
+    // Access livehouse address and lat/long from this value
+    livehouse: { type: Schema.Types.ObjectId, ref: 'Livehouse'},
     name: String, 
-    date: Date,
-    time: Date,
+    datetime: Date,
     description: String,
     genres: [{ type: String }],
-    bands: [{ type: String }],
-    image_url: String,
+    bands: [{ type: Schema.Types.ObjectId, ref: 'Livehouse' }],
     ticket_url: String,
-
-    // Relationships
-    created_by: { type: Schema.Types.ObjectId, ref: 'User' }
+    image_url: String,
+    // Flag whether the gig is scraped or user-created
+    scraped: Boolean,
 }, { timestamps: true });
 
 export default mongoose.model('Gig', GigSchema);
