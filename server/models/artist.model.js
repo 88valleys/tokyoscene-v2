a5b8cf0407d2ff4452f77d30149fd58beb0f47bc
Senie Calalang <senie.c88@gmail.com>
2025-06-23 19:41:05 +0900

    renamed band model to artist

---
+++ server/models/artist.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import User from './models/user.model.js';
+// import Gig from './models/gig.model.js';
+
+const { Schema } = mongoose;
+
+const ArtistSchema = new Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    // TODO: Only implement the below value after the VP is done!
+    // gig: { type: mongoose.Schema.Types.ObjectId, ref: 'Gig', required: true },
+    
+    name: String,
+    description: String,
+    genres: [{ type: String, trim: true }],
+    display_pic: String,
+    band_tracks: [{ type: String }],
+});
+
+export default mongoose.model('Artist', ArtistSchema);
