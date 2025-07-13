772ba20b779a76749cc9986e9ce6a7dfcb965a35
Senie Calalang <senie.c88@gmail.com>
2025-07-13 23:40:59 +0900

    added detailed address, capacity, contact details, socialLinks, scrapeData, events, likedBy and timestamps values

--- server/models/livehouse.model.js
+++ server/models/livehouse.model.js
@@ -1,15 +1,43 @@
 import mongoose from 'mongoose';
 import User from './user.model.js';
+import Event from './event.model.js';
+
+import { GENRES } from '../constants/genres.js';
+import { SOURCE_TYPES } from '../constants/sourceTypes.js';
+import { LIVEHOUSE_SOCIAL_PLATFORMS } from '../constants/socialPlatforms.js';
 
 const { Schema } = mongoose;
 
 const LivehouseSchema = new Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    name: { type: String, trim: true, maxlength: 100, required: true },
 
-    name: String,
-    address: String,                                       
-    description: String,
-    genres: [{ type: String, trim: true }],
+    address: {
+        postalCode: {type: String, trim: true, maxlength: 8},
+        prefecture: {type: String, trim: true, maxlength: 20},
+        city: {type: String, trim: true, maxlength: 100},
+        // 丁目番地号 + building name, in JP or EN
+        street1: {type: String, trim: true, maxlength: 200},
+        // Room number, additional notes, etc.
+        street2: {type: String, trim: true, maxlength: 200}, 
+        country: {
+            type: String,
+            required: true,
+            default: 'Japan',
+            enum: ['Japan']
+        },
+    },   
+
+    phone: { type: String, trim: true, maxlength: 20 },
+    email: { type: String, trim: true, maxlength: 100 },
+    website: { type: String, trim: true, maxlength: 200 },
+
+    capacity: { type: Number, min: 0 },
+
+    socialLinks: [{
+        platform: { type: String, enum: LIVEHOUSE_SOCIAL_PLATFORMS, trim: true },
+        url: { type: String, trim: true }
+    }],
+
     location: {
         type: {
             // Required for GeoJSON
@@ -23,7 +51,27 @@ const LivehouseSchema = new Schema({
             type: [Number], // [longitude, latitude]
             required: true
         }
-    }
-});
+    },
+
+    description: { type: String, trim: true, maxlength: 500, required: true },
+    genres: [{ type: String, trim: true, enum: GENRES }],    
+
+    added_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
+    // Indicate whether the livehouse was added by user or admin
+    source: { type: String, enum: SOURCE_TYPES },
+    // Used only if the source is: "scraped"
+    scrapeData: [{
+        scrapedFrom: { type: String, trim: true },
+        firstScrapedAt: { type: Date },
+        lastScrapedAt: { type: Date },
+        updatedFields: [String],
+    }],
+
+    events: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
+
+    // Track how many users like the livehouse
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
 
 export default mongoose.model('Livehouse', LivehouseSchema);
