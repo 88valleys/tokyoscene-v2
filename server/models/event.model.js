de9a264b4b214b65ad908b49c7837b25f423c6c3
Senie Calalang <senie.c88@gmail.com>
2025-07-13 23:44:31 +0900

    refactor event model to use SOURCE_TYPES constant and enhance price validation

--- server/models/event.model.js
+++ server/models/event.model.js
@@ -5,6 +5,7 @@ import Artist from './artist.model.js';
 import Livehouse from './livehouse.model.js';
 
 import { GENRES } from '../constants/genres.js';
+import { SOURCE_TYPES } from '../constants/sourceTypes.js';
 
 const { Schema } = mongoose;
 
@@ -16,7 +17,7 @@ const EventSchema = new Schema({
     name: { type: String, trim: true, maxlength: 100, required: true },
     doorsOpen: Date,
     endTime: Date,
-    price: Number,
+    price: { type: Number, min: 0, max: 20000 },    
     description: { type: String, trim: true, maxlength: 500, required: true },
     
     genres: [{ type: String, trim: true, enum: GENRES }],    
@@ -26,11 +27,15 @@ const EventSchema = new Schema({
     banner_img: String,
 
     // Flag whether the event is scraped or user-created
-    source: { type: String, enum: ['user', 'scraped', 'admin']},
-    // only used if source is 'scraped'
-    scrapedFrom: { type: String, trim: true },
-    // only used if source is 'scraped' 
-    scrapedTime: Date,
+    source: { type: String, enum: SOURCE_TYPES },
+    // Used only if the source is: "scraped"
+    scrapeData: [{
+        scrapedFrom: { type: String, trim: true },
+        firstScrapedAt: { type: Date },
+        lastScrapedAt: { type: Date },
+        updatedFields: [String],
+    }],
+
 }, { timestamps: true });
 
 export default mongoose.model('Event', EventSchema);
