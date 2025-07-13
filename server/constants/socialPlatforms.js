10ceea200ac081b0fa7d876cfec5f22918536582
Senie Calalang <senie.c88@gmail.com>
2025-07-13 17:37:30 +0900

    add socialPlatforms constants for user and artist

---
+++ server/constants/socialPlatforms.js
@@ -0,0 +1,8 @@
+export const USER_SOCIAL_PLATFORMS = [
+    'instagram', 'twitter', 'facebook', 'tiktok', 'threads', 'spotify', 'discord',
+];
+
+export const ARTIST_SOCIAL_PLATFORMS = [
+    'instagram', 'youtube', 'apple music', 'spotify', 'twitter', 'facebook',
+    'tiktok', 'soundcloud', 'bandcamp', 'website', 'threads', 'patreon', 'discord',
+];
